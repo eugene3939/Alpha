ed5c7bbdb896d6766f323320b337f267e79d4092
2301081 <2301081@systex.com.tw>
2024-01-16 10:44:00 +0800

    spinner 設置

--- app/src/main/java/com/example/alpha/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/alpha/ui/home/HomeFragment.kt
@@ -1,14 +1,19 @@
 package com.example.alpha.ui.home
 
+import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.alpha.databinding.FragmentHomeBinding
 import com.example.alpha.R
+import com.example.alpha.ui.dbhelper.UserDBHelper
 
 class HomeFragment : Fragment() {
 
@@ -17,6 +22,13 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    //product容器
+    private lateinit var dbrw: SQLiteDatabase
+    //table的欄位名稱
+    private var nowColumns: MutableList<String> = mutableListOf()
+    // 在類別內部宣告一個空的List<String>用來存放欄位名稱
+    private val data: MutableList<String> = mutableListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,7 +46,35 @@ class HomeFragment : Fragment() {
             textView.text = it
         }
 
-        val tableNames = resources.getStringArray(R.array.tableNames)
+        // 初始化商品資料庫
+        val dbHelper = UserDBHelper(requireContext())
+        dbrw = dbHelper.writableDatabase
+
+        //預設資料庫索引為User (0)
+        var nowTableId = 0
+
+        //下拉式選單顯示全部的table種類
+        val tablesArray = resources.getStringArray(R.array.tableNames)  //全部的table種類
+        val spinnerAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1,tablesArray)
+        binding.spTableName.adapter = spinnerAdapter
+
+        //下拉式選單變更選擇的資料庫
+        binding.spTableName.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                //更新所在資料庫索引
+                nowTableId = position
+
+                val selectedItem = parent?.getItemAtPosition(position)    //取得選擇的資料
+                //更新GridView顯示所在資料庫內容
+                //updateGridView(nowDBid,null)
+
+                Log.d("目前所在的Table索引是", "索引: $selectedItem")
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
 
         return root
     }
