8030dbb623f732b688e94446a73d76d1d18ba7fa
2301081 <2301081@systex.com.tw>
2024-01-18 11:02:00 +0800

    版面調整2

--- app/src/main/java/com/example/alpha/ui/myObject/ShopCart.kt
+++ app/src/main/java/com/example/alpha/ui/myObject/ShopCart.kt
@@ -1,13 +1,5 @@
 package com.example.alpha.ui.myObject
 
-import android.app.AlertDialog
-import android.content.Context
-import android.view.LayoutInflater
-import android.view.View
-import android.widget.EditText
-import com.example.alpha.R
-
-
 class ShopCart {
     val selectedProducts: MutableList<ProductItem> = mutableListOf()
 
@@ -16,7 +8,7 @@ class ShopCart {
 
         if (existingProduct != null) {
             // 商品已在購物車中，更新數量
-            existingProduct.selectedQuantity += quantity
+            existingProduct.selectedQuantity = quantity
         } else {
             // 商品不在購物車中，添加到購物車
             val productCopy = product.copy(selectedQuantity = quantity)
@@ -34,28 +26,5 @@ class ShopCart {
 
     fun findProductById(products: List<ProductItem>, productId: Int): ProductItem? {
         return products.find { it.pId == productId }
-    }
-
-    fun showQuantityInputDialog(context: Context, onQuantityEntered: (Int) -> Unit) {
-        val builder = AlertDialog.Builder(context)
-        val inflater = LayoutInflater.from(context)
-        val dialogView: View = inflater.inflate(R.layout.checknumber, null)
-
-        val quantityEditText: EditText = dialogView.findViewById(R.id.edt_click_num)
-
-        builder.setView(dialogView)
-            .setTitle("Enter Quantity")
-            .setPositiveButton("OK") { _, _ ->
-                val quantityStr = quantityEditText.text.toString()
-                if (quantityStr.isNotBlank()) {
-                    val quantity = quantityStr.toInt()
-                    onQuantityEntered(quantity)
-                }
-            }
-            .setNegativeButton("Cancel") { dialog, _ ->
-                dialog.dismiss()
-            }
-
-        builder.create().show()
     }
 }
