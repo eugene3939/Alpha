e4106b7181d0b94aafcb8254641e1b145e8836ea
2301081 <2301081@systex.com.tw>
2024-02-26 15:23:14 +0800

    ftp 儲存內部私有空間

--- app/src/main/java/com/example/alpha/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/alpha/ui/notifications/NotificationsFragment.kt
@@ -1,5 +1,6 @@
 package com.example.alpha.ui.notifications
 
+import android.R.attr
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
@@ -11,6 +12,7 @@ import androidx.fragment.app.Fragment
 import com.example.alpha.ConnectionHelper
 import com.example.alpha.databinding.FragmentNotificationsBinding
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.MainScope
@@ -18,8 +20,19 @@ import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 import org.apache.commons.net.ftp.FTP
 import org.apache.commons.net.ftp.FTPClient
+import java.io.BufferedOutputStream
+import java.io.File
+import java.io.FileOutputStream
 import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.nio.file.StandardCopyOption
 import java.sql.Connection
+import android.R.attr.path
+
+import android.os.Environment
+import java.io.FileNotFoundException
+
 
 class NotificationsFragment : Fragment(), CoroutineScope by MainScope() {
 
@@ -34,6 +47,7 @@ class NotificationsFragment : Fragment(), CoroutineScope by MainScope() {
     var connect: Connection? = null
     var ConnectionResult = ""
 
+    @OptIn(DelicateCoroutinesApi::class)
     @SuppressLint("SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -57,17 +71,52 @@ class NotificationsFragment : Fragment(), CoroutineScope by MainScope() {
     private fun connectFTP() {
         ftpClient = FTPClient()
         try {
-            //ftpClient.connect("192.168.91.1", 21)
-            //ftpClient.login("eugene", "eugenemiku")
-            ftpClient.connect("10.60.200.22",21)
-            ftpClient.login("tester","eugenemiku")
+            ftpClient.connect("192.168.91.1", 21)
+            ftpClient.login("eugene", "eugenemiku")
+            //ftpClient.connect("10.60.200.15",21)
+            //ftpClient.login("tester","eugenemiku")
             ftpClient.enterLocalPassiveMode()
             ftpClient.setFileType(FTP.BINARY_FILE_TYPE)
 
             // FTP連接成功
             Log.d("FTP 連線成功", "Connected to FTP server")
 
-            // FTP操作，下載或上傳檔案
+            // 切換到遠端目錄
+            ftpClient.changeWorkingDirectory("/POSDown/FullFile")
+
+            // 列出遠端目錄中的文件列表
+            val files = ftpClient.listFiles()
+            for (file in files) {
+                // 打印文件名稱
+                Log.d("遠端文件", file.name)
+
+                // 2. 創建 DevicePOS 資料夾
+                val devicePOSDirectory = File(requireContext().filesDir, "DevicePOS")
+                if (!devicePOSDirectory.exists()) {
+                    devicePOSDirectory.mkdirs() // 如果目錄不存在，則創建它
+                }else{
+                    Log.d("絕對路徑名稱",devicePOSDirectory.absolutePath)
+                }
+
+                // 3. 下載檔案到 DevicePOS 資料夾中
+                val localFile = File(devicePOSDirectory, file.name)
+                val outputStream = FileOutputStream(localFile)
+
+                // 下載遠端檔案並寫入到本地檔案
+                ftpClient.retrieveFile(file.name, outputStream)
+
+                Log.d("下載成功", file.name)
+
+                if (devicePOSDirectory.exists() && devicePOSDirectory.isDirectory) {
+                    val j = devicePOSDirectory.listFiles()
+                    for (i in j!!) {
+                        Log.d("下載目錄", i.name)
+                    }
+                } else {
+                    Log.d("DevicePOS", "Directory does not exist or is not a directory")
+                }
+            }
+
         } catch (e: IOException) {
             // FTP連接失敗
             Log.e("FTP 連線失敗", "Failed to connect to FTP server: ${e.message}")
@@ -100,6 +149,32 @@ class NotificationsFragment : Fragment(), CoroutineScope by MainScope() {
             Log.e("連線錯誤(kt)", ex.message!!)
         }
     }
+
+    // 將源目錄複製到目標目錄
+    fun copyDirectory(sourceDirPath: String, targetDirPath: String) {
+        val sourceDir = File(sourceDirPath)
+        val targetDir = File(targetDirPath)
+
+        if (!sourceDir.exists() || !sourceDir.isDirectory) {
+            throw IllegalArgumentException("資料不存在")
+        }
+
+        if (!targetDir.exists()) {
+            targetDir.mkdirs()
+        }
+
+        Files.walk(Paths.get(sourceDirPath)).forEach { sourcePath ->
+            val relativePath = sourceDir.toPath().relativize(sourcePath)
+            val targetPath = Paths.get(targetDirPath, relativePath.toString())
+
+            if (Files.isDirectory(sourcePath)) {
+                Files.createDirectories(targetPath)
+            } else {
+                Files.copy(sourcePath, targetPath, StandardCopyOption.REPLACE_EXISTING)
+            }
+        }
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
