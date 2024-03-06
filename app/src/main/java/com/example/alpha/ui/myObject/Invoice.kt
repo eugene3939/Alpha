ecd75466266873ed175de3c4453498680897b60b
2301081 <2301081@systex.com.tw>
2024-03-06 09:38:20 +0800

    小調整

--- app/src/main/java/com/example/alpha/ui/myObject/Invoice.kt
+++ app/src/main/java/com/example/alpha/ui/myObject/Invoice.kt
@@ -6,13 +6,14 @@ import androidx.room.PrimaryKey
 // 定義發票類別
 @Entity(tableName = "InvoiceTable")
 data class Invoice(
-                   @PrimaryKey
-                   val id: String,
-                   val paymentIds: List<String>,
-                   val itemList: List<String>,
-                   val totalPrice: Int,
-                   val discount: Int)
-
+    @PrimaryKey
+    val id: String,
+    @JvmField
+    val paymentIds: List<String>,
+    val itemList: List<String>,
+    val totalPrice: Int,
+    val discount: Int
+)
 
 
 
